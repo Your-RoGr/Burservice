d33a180beb481a7a0d417f87613129b7fa4a712d
Your-RoGr <96972624+Your-RoGr@users.noreply.github.com>
2023-04-23 21:21:26 +0500

    v0.2.1

--- Server/WebApplication1/Controllers/ListController.cs
+++ Server/WebApplication1/Controllers/ListController.cs
@@ -1,45 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Models;
-using WebApplication1.Services;
 
 namespace WebApplication1.Controllers;
 
+// Помечаем класс как контроллер
 [ApiController]
 public class ListController : ControllerBase
 {
-    public const int MAX_FILE_SIZE = 1024 * 8;
-
-    private FileService _fileService;
-
-    public ListController(FileService fileService)
-    {
-        _fileService = fileService;
-    }
-    
-    [HttpPost]
-    [Route("read")]
-    public async Task<IActionResult> ReadFile(IFormFile file)
-    {
-        if (file.Length > MAX_FILE_SIZE)
-        {
-            return BadRequest(new { Message = $"Max file size is {MAX_FILE_SIZE} bytes" });
-        }
-
-        var text = _fileService.ReadFile(file);
-
-        var result = text
-            .Split('\n')
-            .Select(e => e.Trim())
-            .ToList();
-        
-        return Ok(new { List = result });
-    }
-
+    // Помечаем метод как POST запрос по урлу [host]/sort
     [HttpPost]
     [Route("sort")]
-    public async Task<IActionResult> Sort(/*string order, */[FromBody]string[] input)
+    public async Task<IActionResult> Sort([FromQuery]string order, [FromBody]string[] input)
     {
         var list = new MyList<string>(input);
-        return Ok(new { List = list.HeapSort() });
+
+        switch (order)
+        {
+            case "asc":
+                return Ok(list.HeapSort());
+            case "desc":
+                var temp = list.HeapSort();
+                list.InnerEnumerable = temp;
+                return Ok(list.Reverse());
+            case "reverse":
+                return Ok(list.Reverse());
+        }
+
+        return BadRequest("Not supported order");
     }
 }
