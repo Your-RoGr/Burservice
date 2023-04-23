483c2f90dd897fc5d5b0d9487d5784145a2393a2
Evdokimov Andrey <115878885+EvdokimovAndrey@users.noreply.github.com>
2023-04-23 23:12:46 +0500

    Update App.js

--- Client/web-application1/src/App.js
+++ Client/web-application1/src/App.js
@@ -1,14 +1,13 @@
-// Импортируем библиотеки и компоненты
+// Импортируем библиотеки и компоненты 
 import React, { useState } from "react"; 
-
-// Создаем функциональный компонент App 
+ 
+// Создаем функциональный компонент App  
 const App = () => { 
-
-    // Создаем состояния с помощью хука useState
   const [textValue, setTextValue] = useState(""); 
   const [selectedSortMethod, setSelectedSortMethod] = useState(""); 
+  const [sortedList, setSortedList] = useState(""); 
  
-  // Обработчик события изменения текстового поля
+   // Обработчик события изменения текстового поля 
   const handleChange = (e) => { 
     const file = e.target.files[0]; 
  
@@ -24,112 +23,124 @@ const App = () => {
     reader.onerror = (e) => alert(e.target.error.name); 
     reader.readAsText(file); 
   }; 
-
-  // Обработчик события изменения метода сортировки
+ 
+  // Обработчик события изменения метода сортировки 
   const handleSortChange = (e) => { 
-    setTextValue(e.target.value); 
+    setSelectedSortMethod(e.target.value); 
   }; 
  
-  // Отправка запроса на сервер
+  // Отправка запроса на сервер 
   const sortList = (fileContent) => { 
-    fetch("http://localhost:5000/sort", { 
+    console.log(JSON.stringify(JSON.stringify(fileContent.split("\r\n")))); 
+    console.log(selectedSortMethod); 
+ 
+    // определение пути отправки в зависимости от выбранного метода сортировки 
+    const url = 
+      selectedSortMethod === "asc" 
+        ? "http://localhost:5000/sort?order=asc" 
+        : selectedSortMethod === "desс" 
+        ? "http://localhost:5000/sort?order=desc" 
+        : selectedSortMethod === "reverse" 
+        ? "http://localhost:5000/sort?order=reverse" 
+        : ""; 
+ 
+    fetch(url, { 
       method: "POST", 
       headers: { 
         "Content-Type": "application/json", 
       }, 
-      body: JSON.stringify({ fileContent }), 
+      body: JSON.stringify(fileContent.split("\r\n")), 
     }) 
       .then((response) => { 
+        response.json().then((data) => { 
+          console.log(data); 
+          setSortedList(data); 
+        }); 
         console.log("Sort successfully"); 
       }) 
       .catch((error) => { 
         console.error("Error sorting file:", error); 
       }); 
   }; 
-
-  // стили для текстового поля
-  const textAreaStyle = {
+ 
+  // стили для текстового поля 
+  const textAreaStyle = { 
     marginTop: 15, 
     marginBottom: 20, 
-    width: "100%" , 
-    resize: 'vertical', 
-    height: '100vh', 
-    width: "45vw"
-  }
-
-  // стили контейнера для списков
-  const layotStyle = {
-    display: 'flex', 
-    flexDirection: 'row', 
-    justifyContent: 'center',  
-    height: '100vh'
-  }
-
-  // Возвращаем разметку приложения
+    resize: "vertical", 
+    height: "100vh", 
+    width: "45vw", 
+  }; 
+ 
+  // стили контейнера для списков 
+  const layotStyle = { 
+    display: "flex", 
+    flexDirection: "row", 
+    justifyContent: "center", 
+    height: "100vh", 
+  }; 
+ 
+  // Возвращаем разметку приложения 
   return ( 
-    <>
-    <div align="center"> 
-      <input type="file" name="input" onChange={handleChange} /> 
-      <select 
-        value={textValue} 
-        onChange={handleSortChange} 
-        style={{ marginTop: 15, marginLeft: 15 }}
-      > 
-        <option value="ask">ask</option> 
-        <option value="desс">desс</option> 
-        <option value="reverse">reverse</option> 
-      </select>
-      <button 
-        value={textValue} 
-        onChange={handleSortChange}
-        style={{ marginLeft: 15 }} 
-      >
-      Отсортировать  
-      </button>
-    </div> 
-
-    <div style={layotStyle}>
-      <div style={{marginRight: '10px', marginLeft: '10px'}}>
-        <form>
-          <dl>
-            <dt><label for="title">Исходный список</label></dt>
-              <dt>      
+    <> 
+      <div align="center"> 
+        <input type="file" name="input" onChange={handleChange} /> 
+        <select 
+          value={selectedSortMethod} 
+          onChange={handleSortChange} 
+          style={{ marginTop: 15, marginLeft: 15 }} 
+        > 
+          <option value="asc">ask</option> 
+          <option value="desс">desс</option> 
+          <option value="reverse">reverse</option> 
+        </select> 
+        <button onClick={() => sortList(textValue)} style={{ marginLeft: 15 }}> 
+          Отсортировать 
+        </button> 
+      </div> 
+ 
+      <div style={layotStyle}> 
+        <div style={{ marginRight: "10px", marginLeft: "10px" }}> 
+          <form> 
+            <dl> 
+              <dt> 
+                <label htmlFor="title">Исходный список</label> 
+              </dt> 
+              <dt> 
                 <textarea 
                   id="title" 
-                  name="title"
-                  // cols={40} 
-                  // rows={50} 
+                  name="title" 
                   value={textValue} 
-                  onChange={setTextValue} 
-                  style={textAreaStyle} >
-                </textarea>
-              </dt>
-          </dl>
-        </form>
-      </div>
-
-      <div style={{marginLeft: '10px', marginRight: '10px'}}>
-        <form>
-          <dl>
-            <dt><label for="title">Отсортированный список</label></dt>
-              <dt>      
+                  onChange={(e) => setTextValue(e.target.value)} 
+                  style={textAreaStyle} 
+                ></textarea> 
+              </dt> 
+            </dl> 
+          </form> 
+        </div> 
+ 
+        <div style={{ marginLeft: "10px", marginRight: "10px" }}> 
+          <form> 
+            <dl> 
+              <dt> 
+                <label htmlFor="title">Отсортированный список</label> 
+              </dt> 
+              <dt> 
                 <textarea 
                   id="title" 
-                  name="title"
-                  // cols={40} 
-                  // rows={50} 
-                  value={textValue} 
-                  onChange={setTextValue} 
-                  style={textAreaStyle} >
-                </textarea>
-              </dt>
-          </dl>
-        </form>
-      </div>
-    </div>
-    </>
+                  name="title" 
+                  value={sortedList} 
+                  onChange={(e) => setSortedList(e.target.value)} 
+                  style={textAreaStyle} 
+                ></textarea> 
+              </dt> 
+            </dl> 
+          </form> 
+        </div>
+        </div> 
+    </> 
   ); 
 }; 
-
-// Экспортируем компонент App
+ 
+// Экспортируем компонент App 
 export default App;
